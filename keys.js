8f99a8434addeb35cb24eaa92cf4388df84c7a87
Harsh <harsh@harshbaid.com>
2015-09-27 07:35:42 -0400

    GetReq

--- keys.js
+++ keys.js
@@ -10,17 +10,31 @@ var mapping = {
 
 };
 
+$(document).ready(function(){
+    $("#hide").click(function(){
+        $("p").hide();
+    });
+    $("#show").click(function(){
+        $("p").show();
+    });
+});
+
+
 function connect(value){
   console.log(value);
-  if(value){ document.getElementById('yes').style.display = 'inherit'; document.getElementById('no').style.display = 'none'; }
-  else if(!value){ document.getElementById('yes').style.display = 'none'; document.getElementById('no').style.display = 'inherit'; }
+  if(value){ document.getElementById('yes').style.display = 'block'; document.getElementById('no').style.display = 'none'; }
+  else if(!value){ document.getElementById('yes').style.display = 'none'; document.getElementById('no').style.display = 'block'; }
 }
 
 function getreq(direction){
     var req = new XMLHttpRequest(), url = '/key/' + direction;
     req.onreadystatechange = function() {
-        if (req.readyState == 4 && req.status == 200)
-            connect(req.responseText);
+        if (req.readyState == 4 && req.status == 200){
+            $(document).ready(function(){
+              if(req.responseText === 'true'){ $("#no").hide(); $("#yes").show();  }
+              else if(req.responseText === 'false'){ $("#yes").hide(); $("#no").show();  }
+            });
+        }
     }
     req.open("GET", url, true); // true for asynchronous
     req.send(null);
